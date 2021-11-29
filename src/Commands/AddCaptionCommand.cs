b985256a9f5316bc97e74659e1a8c2293c9ea399
Hendrik Bulens <hendrik.bulens@gmail.com>
2021-11-29 14:09:28 +0100

    Captions #15

---
+++ src/Commands/AddCaptionCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Dime.Scheduler.Sdk;
+using Dime.Scheduler.Sdk.Import;
+using Task = System.Threading.Tasks.Task;
+
+namespace Dime.Scheduler.CLI.Commands
+{
+    public class AddCaptionCommand : ICommand<AddCaptionOptions>
+    {
+        public async Task ProcessAsync(AddCaptionOptions options)
+        {
+            try
+            {
+                Console.WriteLine($"Adding caption in context {options.Context} with field {options.FieldName} and value {options.Text} in language {options.Language}");
+
+                IAuthenticator authenticator = new FormsAuthenticator(options.Uri, options.User, options.Password);
+                DimeSchedulerClient client = new(options.Uri, authenticator);
+
+                IImportEndpoint importEndpoint = await client.Import.Request();
+
+                Caption caption = new()
+                {
+                    Context = options.Context,
+                    FieldName = options.FieldName,
+                    Language = options.Language,
+                    SourceTable = options.SourceTable,
+                    Text = options.Text
+                };
+
+                ImportSet result = await importEndpoint.ProcessAsync(caption, TransactionType.Append);
+                Console.WriteLine(result.Success ? "Finished successfully" : "Did not complete successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred: " + ex.Message);
+            }
+        }
+    }
+}
